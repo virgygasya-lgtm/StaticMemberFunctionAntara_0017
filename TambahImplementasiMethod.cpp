f3b3c2974f43f043e83ffe5fa583b52f8f1d4ab5
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-06 17:48:29 +0700

    TambahImplementasiMethod.cpp

--- TambahImplementasiMethod.cpp
+++ TambahImplementasiMethod.cpp
@@ -20,3 +20,17 @@ public:
         setID();
     }
 };
+
+// memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 5;
+
+// mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+}
