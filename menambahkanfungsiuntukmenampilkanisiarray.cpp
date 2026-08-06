f5462e718acb1f2b81d8b09693aa593ceb69e358
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-06 17:04:51 +0700

    menambahkanfungsiuntukmenampilkanisiarray.cpp

--- menambahkanfungsiuntukmenampilkanisiarray.cpp
+++ menambahkanfungsiuntukmenampilkanisiarray.cpp
@@ -26,3 +26,11 @@ angka::~angka(){ // Destructor
     delete[] arr;
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
