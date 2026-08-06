630108f1a999f861624754abcbd018acd39755a0
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-06 17:47:19 +0700

    TambahClassMahasiswa.cpp

---
+++ TambahClassMahasiswa.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa { // membuat class mahasiswa
+
+public:
+    static int nim; // deklarasi variabel static yaitu variabel nim
+
+    // deklarasi variabel member
+    int id;
+    string nama;
+
+    // deklarasi prosedur setID() dan printAll()
+    void setID();
+    void printAll();
+
+    // pembuatan constructor Mahasiswa dengan parameter pnama
+    // member initialize list
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    }
+};
