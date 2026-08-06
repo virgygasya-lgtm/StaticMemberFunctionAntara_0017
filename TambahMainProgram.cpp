83f9e41443f141dd95c1975e521278563ced6e34
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-06 17:43:08 +0700

    TambahMainProgram.cpp

--- TambahMainProgram.cpp
+++ TambahMainProgram.cpp
@@ -40,3 +40,31 @@ void Mahasiswa::printAll() {
     cout << "Nama = " << nama << endl;
     cout << endl;
 }
+
+int main()
+{
+    // pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa::setNim(10);
+    Mahasiswa mhs2("Budi Jatmiko");
+
+    // memberi nilai pada setNim() untuk merubah nilai NIM
+    // Mahasiswa::setNim(9);
+
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    // memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    // menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = "
+         << Mahasiswa::getNim() << endl;
+
+    system("pause");
+
+    return 0;
+}
