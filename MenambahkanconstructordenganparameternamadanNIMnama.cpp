d53ed4eed899c20f7892fb478c37c67ca0f5d5b1
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-06 17:34:18 +0700

    MenambahkanconstructordenganparameternamadanNIMnama.cpp

--- MenambahkanconstructordenganparameternamadanNIMnama.cpp
+++ MenambahkanconstructordenganparameternamadanNIMnama.cpp
@@ -21,3 +21,12 @@ mahasiswa::mahasiswa(){
 mahasiswa::mahasiswa(int iNim){
     nim = iNim; // definisi hanya NIM
 }
+
+mahasiswa::mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
