7291ed24321e2008d4c8fe4bd01f0afb5c508dc1
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-06 17:40:56 +0700

    TambahLibraryDanClassMahasiswa.cpp

---
+++ TambahLibraryDanClassMahasiswa.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string> // menyertakan library string
+using namespace std;
+
+class Mahasiswa {
+
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    // pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    }
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    }
+};
